81b7512f3e501a21300d1826903704b878fd1401
Julian Sanchez <jasanchezr.utbb@gmail.com>
2025-07-28 11:15:19 +0000

    Error: Not in a Firebase app directory (could not locate firebase.json)

--- functions/src/genkit-sample.ts
+++ functions/src/genkit-sample.ts
@@ -1,6 +1,10 @@
 // Import the Genkit core libraries and plugins.
 import {genkit, z} from "genkit";
+import {googleAI} from "@genkit-ai/googleai";
 
+// Import models from the Google AI plugin. The Google AI API provides access to
+// several generative models. Here, we import Gemini 2.0 Flash.
+import {gemini20Flash} from "@genkit-ai/googleai";
 
 // Cloud Functions for Firebase supports Genkit natively. The onCallGenkit function creates a callable
 // function from a Genkit action. It automatically implements streaming if your flow does.
@@ -21,7 +25,11 @@ enableFirebaseTelemetry();
 
 const ai = genkit({
   plugins: [
-    /* Add your plugins here. */
+    // Load the Google AI plugin. You can optionally specify your API key
+    // by passing in a config object; if you don't, the Google AI plugin uses
+    // the value from the GOOGLE_GENAI_API_KEY environment variable, which is
+    // the recommended practice.
+    googleAI(),
   ],
 });
 
@@ -36,7 +44,7 @@ const menuSuggestionFlow = ai.defineFlow({
     const prompt =
       `Suggest an item for the menu of a ${subject} themed restaurant`;
     const { response, stream } = ai.generateStream({
-      model: '' /* TODO: Set a model. */,
+      model: gemini20Flash,
       prompt: prompt,
       config: {
         temperature: 1,
