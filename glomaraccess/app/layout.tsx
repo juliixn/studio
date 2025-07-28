c9f52c782ac8dd30ec1d7e1bb48adc380716c5c9
Julian Sanchez <jasanchezr.utbb@gmail.com>
2025-07-28 10:06:58 +0000

    import { Geist } from 'next/font/google'
    const geist = Geist({
      subse

--- glomaraccess/app/layout.tsx
+++ glomaraccess/app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geist = Geist({
   subsets: ["latin"],
 });
 
@@ -23,10 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="en" className={geist.className}>
+      <body>
         {children}
       </body>
     </html>
